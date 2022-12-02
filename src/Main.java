cd0cfa852ab4b7a70a60cf1fb886f94171d45970
Artur <artur.augustyn26@gmail.com>
2022-12-02 10:43:58 +0100

    added class Person, added startMethod, added newPerson Method and processQueue method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        PersonQueue personQueue = new PersonQueue();
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String name = scanner.nextLine();
+            personQueue.StartMethod(name);
+        }
 
     }
 }
